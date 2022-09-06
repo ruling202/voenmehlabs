0d84018e54a4d4e8ec88705bfd289c2a453da193
ruling202 <kreckertuc02@gmail.com>
2022-09-06 15:47:46 +0300

    Лаб-1 Ткачев ВИ921Б
    Лаб-1 Ткачев ВИ921Б

---
+++ Лаб-1 Ткачев ВИ921Б.c
@@ -0,0 +1,8 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include <stdio.h>
+int main (void) {
+    printf ("Ткачев, Матвей, Вадимович, ВИ921Б");
+    return 0;
+}
