3e839497d4d351182b05a8e15218e54d8a224a3c
ruling202 <112892909+ruling202@users.noreply.github.com>
2022-10-11 10:24:59 +0300

    Add files via upload

---
+++ Types dannyie.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int a=0x15;
+   printf("%d\n",a);
+   printf("%f\n",a);
+   printf("%li\n",a);
+   
+   int i = -1;
+   printf("%d\n",i);
+   printf("%u\n",i);
+   
+   double n = 0.1234;
+   printf("%d\n",n);
+   printf("%u\n",n);
+   
+
+    return 0;
+}
